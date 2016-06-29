f8e1845c7778a457ec82e7b0708ec0eddfbd96bb
Soraaaa <leduythanh1997@gmail.com>
2016-06-30 00:46:14 +1000

    Update Helper.cs

--- Brian Sharp/Common/Helper.cs
+++ Brian Sharp/Common/Helper.cs
@@ -1,14 +1,19 @@
-﻿namespace BrianSharp.Common
+using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy.SDK.Enumerations;
+using SharpDX;
+    
+namespace BrianSharp.Common
 {
-    using System.Collections.Generic;
-    using System.Linq;
-
-    using LeagueSharp;
-    using LeagueSharp.Common;
-    using LeagueSharp.Common.Data;
-
-    using SharpDX;
-
     internal class Helper : Program
     {
         #region Enums
