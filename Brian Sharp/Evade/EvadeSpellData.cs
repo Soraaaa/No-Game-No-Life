ca7f0a9459d9dafabda2c3bbbd3ab81d3fe9303f
Soraaaa <leduythanh1997@gmail.com>
2016-06-30 07:36:36 +1000

    Update EvadeSpellData.cs

--- Brian Sharp/Evade/EvadeSpellData.cs
+++ Brian Sharp/Evade/EvadeSpellData.cs
@@ -1,11 +1,17 @@
     using BrianSharp.Common;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
     using EloBuddy;
     using EloBuddy.SDK;
+    using EloBuddy.SDK.Enumerations;
     using EloBuddy.SDK.Events;
     using EloBuddy.SDK.Menu;
     using EloBuddy.SDK.Menu.Values;
     using EloBuddy.SDK.Rendering;
-    using EloBuddy.SDK.Enumerations;
+    using SharpDX;
 
 namespace BrianSharp.Evade
 {
